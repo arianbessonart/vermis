89ee381f60a2afdd5fae0ae8ceaadcf6c8de4385
arianbessonart <arianbessonart@gmail.com>
2014-10-25 21:15:16 -0200

    redirect home to clients

--- vermis/urls.py
+++ vermis/urls.py
@@ -6,6 +6,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'vermis.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^', include('clients.urls', namespace='clients')),
     url(r'^clients/', include('clients.urls', namespace='clients')),
     url(r'^invoices/', include('invoices.urls', namespace='invoices')),
     url(r'^admin/', include(admin.site.urls)),
